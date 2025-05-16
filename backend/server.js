02123548291d855f126e9d0dd9385509c1da6705
Prateekbuthale <131975203+Prateekbuthale@users.noreply.github.com>
2025-05-17 01:45:22 +0530

    projects db querying added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import authRoutes from './routes/authRoutes.js';
-import embeddingRoutes from './routes/embeddingRoutes.js'
-import pineconeRoutes from './routes/pineconeRoutes.js'
+import authRoutes from "./routes/authRoutes.js";
+import embeddingRoutes from "./routes/embeddingRoutes.js";
+import pineconeRoutes from "./routes/pineconeRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
 
 dotenv.config();
 
@@ -15,9 +16,10 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/embeddings', embeddingRoutes)
-app.use('/pinecone', pineconeRoutes)
+app.use("/auth", authRoutes);
+app.use("/embeddings", embeddingRoutes);
+app.use("/pinecone", pineconeRoutes);
+app.use("/projects", projectRoutes);
 //app.use('/embedding', embeddingRoutes);
 //app.use('/storage', storageRoutes);
 
